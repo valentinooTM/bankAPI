c3aec25ab5d4017d49a9b349af7329ff14c83383
valentinooTM <maks.wielgomasz@gmail.com>
2023-01-09 09:36:18 +0100

    db

--- bankapi_db.sql
+++ bankapi_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 09 Gru 2022, 11:03
+-- Czas generowania: 09 Sty 2023, 09:35
 -- Wersja serwera: 10.4.21-MariaDB
 -- Wersja PHP: 8.0.10
 
@@ -31,16 +31,17 @@ CREATE TABLE `accounts` (
   `account_num` varchar(26) NOT NULL,
   `name` varchar(30) DEFAULT NULL,
   `surname` varchar(30) DEFAULT NULL,
-  `balance` decimal(8,2) DEFAULT NULL
+  `balance` decimal(8,2) DEFAULT NULL,
+  `password` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Zrzut danych tabeli `accounts`
 --
 
-INSERT INTO `accounts` (`account_num`, `name`, `surname`, `balance`) VALUES
-('11112222', 'sklep', 'sklepJDBC', '20021.23'),
-('123456', 'Maks', 'Wielgomasz', '936.31');
+INSERT INTO `accounts` (`account_num`, `name`, `surname`, `balance`, `password`) VALUES
+('11112222', 'sklep', 'sklepJDBC', '20618.95', 'sklepik123'),
+('123456', 'Maks', 'Wielgomasz', '338.59', 'lechpoznan');
 
 -- --------------------------------------------------------
 
@@ -49,17 +50,25 @@ INSERT INTO `accounts` (`account_num`, `name`, `surname`, `balance`) VALUES
 --
 
 CREATE TABLE `blik_nums` (
-  `blik_num` int(6) NOT NULL,
+  `blik_num` varchar(6) NOT NULL,
   `demanded_money` decimal(8,2) DEFAULT NULL,
-  `account_num` varchar(26) DEFAULT NULL
+  `account_num` varchar(26) DEFAULT NULL,
+  `target_account` varchar(26) DEFAULT NULL,
+  `status` varchar(14) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Zrzut danych tabeli `blik_nums`
 --
 
-INSERT INTO `blik_nums` (`blik_num`, `demanded_money`, `account_num`) VALUES
-(336637, NULL, '123456');
+INSERT INTO `blik_nums` (`blik_num`, `demanded_money`, `account_num`, `target_account`, `status`) VALUES
+('009933', '246.12', '123456', '11112222', 'to_confirm'),
+('247596', '127.78', '123456', '11112222', 'to_confirm'),
+('276244', '204.20', '123456', '11112222', 'expired'),
+('511712', '294.70', '123456', '11112222', 'to_confirm'),
+('523263', '265.49', '123456', '11112222', 'used'),
+('532172', '337.68', '123456', '11112222', 'to_confirm'),
+('702019', '332.23', '123456', '11112222', 'used');
 
 --
 -- Indeksy dla zrzutów tabel
